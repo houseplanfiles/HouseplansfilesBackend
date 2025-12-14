7f1aabbb5f51f5126850376389881f6c777539d7
houseplanfiles <houseplansdesignsfile@gmail.com>
2025-12-14 10:31:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 // ✅ CORS FIX (Vercel)
 // =====================
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://www.houseplanfiles.com");
+  res.header("Access-Control-Allow-Origin", "https://www.houseplanfiles.com","https://houseplansfilesfrontend.vercel.app");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
